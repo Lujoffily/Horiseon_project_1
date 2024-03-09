fdba673dc6ce1c29457fbfde52fe7bc2da174e23
Luna Joffily <luna.joffily@gmail.com>
2024-03-08 23:41:26 -0500

    start on body ajustment on small screens

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,30 @@
 var toggle = document.querySelector('#toggle-btn');
 var menu = document.getElementById('menu');
+var body = document.getElementById('body')
+var aboutId = document.getElementById('AboutBtn');
+var projectId = document.getElementById('ProjectBtn');
+var resumeId = document.getElementById('ResumeBtn');
+var contactId = document.getElementById('ContactBtn');
 
 toggle.onclick = function () {
     menu.classList.toggle('active');
-    return console.log('hello');
+
+    if (menu.classList.contains('active')) {
+        var aboutBtn = document.createTextNode('About');
+        var projectBtn = document.createTextNode('Projects');
+        var resumeBtn = document.createTextNode('Resume');
+        var contactBtn = document.createTextNode('Contact');
+
+        aboutId.appendChild(aboutBtn);
+        projectId.appendChild(projectBtn);
+        resumeId.appendChild(resumeBtn);
+        contactId.appendChild(contactBtn);
+    } else {
+        aboutId.innerHTML = '';
+        projectId.innerHTML = '';
+        resumeId.innerHTML = '';
+        contactId.innerHTML = '';
+    }
 }
 
 document.addEventListener('click', function(event) {
